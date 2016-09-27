e6000aa0a14937185d523bc6b4bff32c36048945
Nathalie Arana Moreno <Nitas@iMac-de-Nitas.local>
2016-09-27 18:46:12 -0500

    Initial commit

---
+++ app/src/main/java/slc/nam/lunes/MainActivity.java
@@ -0,0 +1,22 @@
+package slc.nam.lunes;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBar actionBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
